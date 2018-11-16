9631f736c0f02d255cf62531a2fb58bb0ebfd448
Dane Hillard <github@danehillard.com>
2018-11-16 11:19:29 -0500

    Update talks page (#11)

--- src/views/Talks.vue
+++ src/views/Talks.vue
@@ -3,9 +3,17 @@
         <BaseHeading>
             Talks
         </BaseHeading>
-        <ul>
+        <ul class="talk-list">
             <li>
-                <YouTubeEmbed video-id="ZfynWM4-jFE" />
+                <BaseHeading level="2">Serverless Django with Zappa</BaseHeading>
+                <BaseHeading level="3">DjangoCon US 2018</BaseHeading>
+                <div class="talk-instance__visuals">
+                    <ResponsiveImage
+                        :src="require('@/assets/djangocon.png')"
+                        alt="DjangoCon US 2018"
+                    />
+                    <YouTubeEmbed video-id="ZfynWM4-jFE" />
+                </div>
             </li>
         </ul>
     </main>
@@ -14,6 +22,7 @@
 <script>
 import BaseHeading from '@/components/BaseHeading'
 import YouTubeEmbed from '@/components/YouTubeEmbed'
+import ResponsiveImage from '@/components/ResponsiveImage'
 
 export default {
   name: 'Talks',
@@ -34,16 +43,21 @@ export default {
   },
   components: {
     BaseHeading,
-    YouTubeEmbed
+    YouTubeEmbed,
+    ResponsiveImage
   }
 }
 </script>
 
 <style scoped lang="scss">
-ul {
+.talk-list {
   list-style: none;
+
+}
+
+.talk-instance__visuals {
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: minmax(75px, 250px) minmax(250px, 500px);
   grid-gap: 1rem;
 }
 </style>
