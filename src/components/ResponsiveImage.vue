6253c99fa06c11d0e5b1ef66df115185a5e0b225
Dane Hillard <github@danehillard.com>
2018-11-16 11:18:15 -0500

    Update talks page

---
+++ src/components/ResponsiveImage.vue
@@ -0,0 +1,29 @@
+<template>
+    <img
+        :src="src"
+        :alt="alt"
+    >
+</template>
+
+<script>
+export default {
+  name: 'ResponsiveImage',
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
