57aaa5eee6788f181537a6543f23db0f02adbb36
Dane Hillard <github@danehillard.com>
2018-11-16 19:57:18 -0500

    Add analytics (#13)

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
 import '@/registerServiceWorker'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'UA-129371342-1',
+  router,
+  debug: {
+    enabled: process.env.NODE_ENV !== 'production',
+    sendHitTask: process.env.NODE_ENV === 'production'
+  }
+})
 
 new Vue({
   router,
