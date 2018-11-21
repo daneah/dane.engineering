8ca3e95ba9bcf6896b615a8daafe91d6a94a4c63
Dane Hillard <github@danehillard.com>
2018-11-21 12:30:03 -0500

    Update homepage copy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,19 @@
         <BaseHeading>About Dane</BaseHeading>
 
         <BaseParagraph>
+            Dane grew up in Michigan and currently lives in Pittsburgh, Pennsylvania.
+        </BaseParagraph>
+
+        <BaseParagraph>
             Dane has a B.S.E in computer engineering with a minor in mathematics from the University of Michigan.
             As a software engineer, Dane has worked on research and development at SAIC (now <BaseLink href="https://www.leidos.com/">Leidos</BaseLink>)
             and personalized cancer genomics at Compendia Bioscience (now part of <BaseLink href="https://www.thermofisher.com">Thermo Fisher Scientific</BaseLink>).
             As a web application developer at <BaseLink href="https://www.ithaka.org">ITHAKA</BaseLink>,
-            his focus has been mainly on web development in Python and Django to enable academic research.
+            his focus is mainly on web development in Python, Django, and front-end technologies to enable academic research.
         </BaseParagraph>
 
         <BaseParagraph>
-            Dane Hillard is an engineer, musician, photographer, dancer, and creative.
+            Dane is an engineer, musician, photographer, dancer, and creative.
             Contribute to his rampant serial hobbyism by reaching out about stuff you want to build together.
         </BaseParagraph>
     </main>
