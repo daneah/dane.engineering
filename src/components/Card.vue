17d3d9416b4e1e2be8c0e6a4fdebb2f65d9475eb
Dane Hillard <github@danehillard.com>
2018-11-28 15:39:53 -0500

    Add card hover styling (#45)

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -35,7 +35,16 @@ export default {
 
 <style scoped lang="scss">
 .card {
+  --transition-time: 0.25s;
+  --transition-easing: ease-in-out;
+
   border: 1px solid var(--gray);
+  transition: transform var(--transition-time) var(--transition-easing), box-shadow var(--transition-time) var(--transition-easing);
+
+  &:hover {
+    transform: translateY(-5px);
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+  }
 }
 
 .card__body {
