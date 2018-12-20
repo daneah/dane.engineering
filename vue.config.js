048b6335267ab438311ea49719bd58399ed4aa73
Dane Hillard <github@danehillard.com>
2018-12-19 20:02:38 -0500

    Prerender site for SEO/Netlify purposes

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const PrerenderSpaPlugin = require('prerender-spa-plugin');
+
+const productionPlugins = [
+  new PrerenderSpaPlugin({
+    staticDir: path.join(__dirname, 'dist'),
+    routes: [
+        '/',
+        '/talks',
+        '/resume',
+        '/contact',
+        '/talks',
+    ],
+  }),
+];
+
+module.exports = {
+  lintOnSave: false,
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(...productionPlugins);
+    }
+  },
+};
