07ec50054e732bc54075e991582164d8f5ef42c5
Dane Hillard <github@danehillard.com>
2019-01-28 19:46:09 -0500

    Brush up descriptions of responsibilities

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,7 @@
 
                 <BaseParagraph>
                     Dane has a B.S.E in computer engineering with a minor in mathematics from the University of Michigan.
-                    As a software engineer, Dane has worked on research and development at SAIC (now <BaseLink href="https://www.leidos.com/">Leidos</BaseLink>)
+                    As a software engineer, Dane has worked on intelligence research and development at SAIC (now <BaseLink href="https://www.leidos.com/">Leidos</BaseLink>)
                     and personalized cancer genomics at Compendia Bioscience (now part of <BaseLink href="https://www.thermofisher.com">Thermo Fisher Scientific</BaseLink>).
                     As a lead web application developer at <BaseLink href="https://www.ithaka.org">ITHAKA</BaseLink>,
                     his focus is mainly on web development in Python, Django, and front-end technologies to enable academic research.
