77bff779e1e506fb600780db63cefca26e7bfd8a
Dane Hillard <github@danehillard.com>
2019-02-03 16:07:34 -0500

    Remove .vue extension in import

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueAnalytics from 'vue-analytics'
-import App from '@/App.vue'
+import App from '@/App'
 import router from '@/router'
 import store from '@/store'
 import '@/registerServiceWorker'
