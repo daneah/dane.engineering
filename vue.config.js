e1988c97f726bebda54d60d9759124759468e12c
Dane Hillard <github@danehillard.com>
2019-04-11 21:47:17 -0400

    Only load latest-posts.json when production build

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,26 @@
 const path = require('path')
 const PrerenderSpaPlugin = require('prerender-spa-plugin')
-const latestPosts = require('./latest-posts.json')
-
-const productionPlugins = [
-  new PrerenderSpaPlugin({
-    renderAfterTime: 5000,
-    staticDir: path.join(__dirname, 'dist'),
-    routes: [
-        '/',
-        '/talks',
-        '/resume',
-        '/contact',
-        '/talks',
-        '/posts',
-        ...latestPosts['data'].map((post) => `/post/${post.slug}`),
-    ],
-  }),
-]
 
 module.exports = {
   lintOnSave: false,
   configureWebpack: (config) => {
     if (process.env.NODE_ENV === 'production') {
+      const latestPosts = require('./latest-posts.json')
+      const productionPlugins = [
+        new PrerenderSpaPlugin({
+          renderAfterTime: 5000,
+          staticDir: path.join(__dirname, 'dist'),
+          routes: [
+            '/',
+            '/talks',
+            '/resume',
+            '/contact',
+            '/talks',
+            '/posts',
+            ...latestPosts['data'].map((post) => `/post/${post.slug}`),
+          ],
+        }),
+      ]
       config.plugins.push(...productionPlugins)
     }
   },
