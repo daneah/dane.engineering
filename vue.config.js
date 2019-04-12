4df1c24fdef0403f7cc01c3cb746ee20bb3c2552
Dane Hillard <github@danehillard.com>
2019-04-11 21:32:45 -0400

    Revert "Prerender all blog posts dynamically (#103)"
    This reverts commit 145332c23d7ebaf5f31b2c680aaec915d199efc2.

--- vue.config.js
+++ vue.config.js
@@ -1,40 +1,29 @@
-const Butter = require('buttercms')
 const path = require('path')
 const PrerenderSpaPlugin = require('prerender-spa-plugin')
 
-let blogPostRoutes = [],
-  productionPlugins = []
+const productionPlugins = [
+  new PrerenderSpaPlugin({
+    renderAfterTime: 5000,
+    staticDir: path.join(__dirname, 'dist'),
+    routes: [
+        '/',
+        '/talks',
+        '/resume',
+        '/contact',
+        '/talks',
+        '/posts',
+        '/post/buttercms-in-vue',
+        '/post/truly-taking-time-off-a-checklist',
+        '/post/flexible-approach-python-api-client-development',
+    ],
+  }),
+]
 
 module.exports = {
   lintOnSave: false,
   configureWebpack: (config) => {
     if (process.env.NODE_ENV === 'production') {
-      const butter = Butter('cd7317f31d717fc87a1374098f53651830003bed')
-      butter.post.list({page: 1, pageSize: 100}).then((response) => {
-        blogPostRoutes = response.data['data'].map((post) => `/post/${post.slug}`)
-      }).catch((err) => {
-        console.error(`Could not fetch posts from ButterCMS! ${err}`)
-      }).finally(() => {
-        const routes = [
-          '/',
-          '/talks',
-          '/resume',
-          '/contact',
-          '/talks',
-          '/posts',
-          ...blogPostRoutes,
-        ]
-
-        productionPlugins = [
-          new PrerenderSpaPlugin({
-            renderAfterTime: 5000,
-            staticDir: path.join(__dirname, 'dist'),
-            routes: routes,
-          }),
-        ]
-
-        config.plugins.push(...productionPlugins)
-      })
+      config.plugins.push(...productionPlugins)
     }
   },
 }
