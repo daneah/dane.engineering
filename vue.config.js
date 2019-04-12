67afa2abe5e9db3fc9e3fe5ff4ffb73e6ef50795
Dane Hillard <github@danehillard.com>
2019-04-11 21:50:05 -0400

    Prerender all blog posts (#104)
    * Revert "Prerender all blog posts dynamically (#103)"
    
    This reverts commit 145332c23d7ebaf5f31b2c680aaec915d199efc2.
    
    * Get post list in prebuild step
    
    * Only load latest-posts.json when production build

--- vue.config.js
+++ vue.config.js
@@ -1,40 +1,27 @@
-const Butter = require('buttercms')
 const path = require('path')
 const PrerenderSpaPlugin = require('prerender-spa-plugin')
-
-let blogPostRoutes = [],
-  productionPlugins = []
 
 module.exports = {
   lintOnSave: false,
   configureWebpack: (config) => {
     if (process.env.NODE_ENV === 'production') {
-      const butter = Butter('cd7317f31d717fc87a1374098f53651830003bed')
-      butter.post.list({page: 1, pageSize: 100}).then((response) => {
-        blogPostRoutes = response.data['data'].map((post) => `/post/${post.slug}`)
-      }).catch((err) => {
-        console.error(`Could not fetch posts from ButterCMS! ${err}`)
-      }).finally(() => {
-        const routes = [
-          '/',
-          '/talks',
-          '/resume',
-          '/contact',
-          '/talks',
-          '/posts',
-          ...blogPostRoutes,
-        ]
-
-        productionPlugins = [
-          new PrerenderSpaPlugin({
-            renderAfterTime: 5000,
-            staticDir: path.join(__dirname, 'dist'),
-            routes: routes,
-          }),
-        ]
-
-        config.plugins.push(...productionPlugins)
-      })
+      const latestPosts = require('./latest-posts.json')
+      const productionPlugins = [
+        new PrerenderSpaPlugin({
+          renderAfterTime: 5000,
+          staticDir: path.join(__dirname, 'dist'),
+          routes: [
+            '/',
+            '/talks',
+            '/resume',
+            '/contact',
+            '/talks',
+            '/posts',
+            ...latestPosts['data'].map((post) => `/post/${post.slug}`),
+          ],
+        }),
+      ]
+      config.plugins.push(...productionPlugins)
     }
   },
 }
