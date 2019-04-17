4bf0b3e3d1a598739b12cb6a4153702fc222872e
Dane Hillard <github@danehillard.com>
2019-04-17 12:12:34 -0400

    Update book title after title change

--- src/components/BookAnnouncement.vue
+++ src/components/BookAnnouncement.vue
@@ -4,7 +4,7 @@
             <h2>I'm writing a book!</h2>
 
             <p>
-                <BaseLink href="https://bit.ly/clap-py"><em>Code Like a Pro: Software Development in Python</em></BaseLink>
+                <BaseLink href="https://bit.ly/the-python-pro"><em>Practices of the Python Pro</em></BaseLink>
                 is currently available through Manning's early access program.
                 I've been sharing my experience in Python with others for a few years
                 and this is my biggest attempt in that endeavor, to date!
@@ -17,14 +17,14 @@
             </p>
 
             <p>
-                The first chapter is available <strong>free of charge</strong> and the early access currently includes the first four chapters.
-                If you <BaseLink href="https://bit.ly/clap-py">buy now</BaseLink> you can read new chapters as they're ready
+                The first chapter is available <strong>free of charge</strong> and the early access currently includes the first five chapters.
+                If you <BaseLink href="https://bit.ly/the-python-pro">buy now</BaseLink> you can read new chapters as they're ready
                 and provide feedback along the way so that I can make this book a great learning tool for everyone!
             </p>
         </div>
         <ResponsiveImage
-            :src="require('@/assets/code-like-a-pro-meap-cover.png')"
-            alt="Book cover for Code Like a Pro: Software Development in Python"
+            :src="require('@/assets/practices-of-the-python-pro-cover.png')"
+            alt="Book cover for Practices of the Python Pro by Dane Hillard"
         />
     </div>
 </template>
