78c150c88651b6b9279ca61bf15aed0175b03971
Dane Hillard <github@danehillard.com>
2019-08-10 11:29:06 -0400

    Fix tests by mocking webp and updating expectations

--- tests/unit/components/ResponsiveImage.spec.js
+++ tests/unit/components/ResponsiveImage.spec.js
@@ -13,4 +13,15 @@ describe('ResponsiveImage', () => {
         })
         expect(wrapper.contains('img[src="test.jpg"][alt="Test image"]')).toBe(true)
     })
+
+    it('Renders a picture with webp source when provided', () => {
+        const wrapper = shallowMount(ResponsiveImage, {
+            propsData: {
+                src: 'test.jpg',
+                webp: 'test.webp',
+                alt: 'Test image'
+            }
+        })
+        expect(wrapper.contains('picture source[srcset="test.webp"]')).toBe(true)
+    })
 })
