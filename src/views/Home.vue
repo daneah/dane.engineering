36a7ada0929b80a979a2fa67831a76c0ffb7621c
Dane Hillard <github@danehillard.com>
2019-10-28 13:01:25 -0400

    Add Product Hunt embed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,16 @@
     <main>
         <h1>About Dane</h1>
 
+        <div v-if="new Date() <= new Date('2019-10-29')">
+            <div>
+                <h2>Practices of the Python Pro is on Product Hunt!</h2>
+                <p>Please join the discussion and write a review!</p>
+            </div>
+            <div class="product-hunt-embed">
+                <iframe style="border: none;" src="https://cards.producthunt.com/cards/posts/172000?v=1" width="500" height="405" frameborder="0" scrolling="no" allowfullscreen></iframe>
+            </div>
+        </div>
+
         <div class="about">
             <div class="bio">
                 <p>
@@ -212,5 +222,12 @@ export default {
 
 .portrait img {
     clip-path: polygon(0% 4%, 95% 0%, 100% 100%, 0% 97%);
+}
+
+.product-hunt-embed {
+    background: white;
+    display: inline-block;
+    padding: 0;
+    margin: 1rem 0;
 }
 </style>
