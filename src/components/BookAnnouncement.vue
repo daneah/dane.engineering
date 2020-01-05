53fee6780b7be17eebd592d601fe0dc0ec6b82c7
Dane Hillard <github@danehillard.com>
2020-01-05 14:28:33 -0500

    Update book announcement to reflect book completion

--- src/components/BookAnnouncement.vue
+++ src/components/BookAnnouncement.vue
@@ -1,23 +1,17 @@
 <template>
     <div class="book-announcement">
         <div class="book-plug">
-            <h2>I'm writing a book!</h2>
+            <h2>I wrote a book!</h2>
 
             <p>
-                <router-link class="link" :to="{'name': 'books'}"><em>Practices of the Python Pro</em></router-link>
-                is currently available through Manning's early access program.
-                I've been sharing my experience in Python with others for a few years
-                and this is my biggest attempt in that endeavor, to date!
+                After much ado, <router-link class="link" :to="{'name': 'books'}"><em>Practices of the Python Pro</em></router-link>
+                is now available for purchase through Manning Books.
             </p>
 
             <p>
                 This book is geared toward people just getting into software development,
                 or using Python as a supplement to their work in a different discipline.
                 Please share with anyone looking to learn more about software development principles and best practices!
-            </p>
-
-            <p>
-                The first chapter is available <strong>free of charge</strong> and the full book is available in early access until production is finalized.
             </p>
         </div>
         <ResponsiveImage
