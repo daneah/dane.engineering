59d9593031f45b5fd1b3d7681ab8e29f00d996eb
Dane Hillard <github@danehillard.com>
2020-01-05 16:24:17 -0500

    Add storybook to static serving (#144)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,14 @@
 import { addParameters, addDecorator } from '@storybook/vue';
 import { withA11y } from '@storybook/addon-a11y';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 import '@/main.scss'
 
 addDecorator(withA11y);
 addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
   docs: {
     iframeHeight: '200px',
     inlineStories: false,
