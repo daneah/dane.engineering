f57a5456ddab3cfc86eb622d36b79244b28d01c5
Dane Hillard <github@danehillard.com>
2020-03-22 12:22:29 -0400

    Add links to external articles

---
+++ src/components/Badge.vue
@@ -0,0 +1,27 @@
+<template>
+    <span
+        class="badge"
+        :style="{'background': color}"
+    >
+        <slot />
+    </span>
+</template>
+
+<script>
+export default {
+  name: "Badge",
+  props: {
+    color: {
+      type: String,
+      default: "var(--blue)",
+    }
+  }
+}
+</script>
+
+<style scoped>
+.badge {
+  padding: 0.35rem;
+  border-radius: 2px;
+}
+</style>
--- src/views/BlogPostList.vue
+++ src/views/BlogPostList.vue
@@ -1,41 +1,93 @@
 <template>
-  <main>
-    <h1>Posts</h1>
-    <p v-if="loading">Loading...</p>
-    <CardGrid v-else-if="posts.data.length">
-      <template #cards>
-        <ul class="posts">
-          <li
-            v-for="post in posts.data"
-            :key="post.slug"
-            class="posts__post"
-          >
-            <Card :link="{ name: 'post', params: { slug: post.slug }}">
-              <template #image>
-                  <ResponsiveImage
-                    v-if="post.featured_image"
-                    :src="postThumbnail(post.featured_image)"
-                    alt=""
-                  />
-              </template>
-              <template #title>
-                  {{ post.title }}
-              </template>
-              <template #description>{{ post.summary }}</template>
-            </Card>
-          </li>
+    <main>
+        <h1>Posts</h1>
+        <p v-if="loading">
+            Loading...
+        </p>
+        <CardGrid v-else-if="posts.data.length">
+            <template #cards>
+                <ul class="posts">
+                    <li
+                        v-for="post in posts.data"
+                        :key="post.slug"
+                        class="posts__post"
+                    >
+                        <Card :link="{ name: 'post', params: { slug: post.slug }}">
+                            <template #image>
+                                <ResponsiveImage
+                                    v-if="post.featured_image"
+                                    :src="postThumbnail(post.featured_image)"
+                                    alt=""
+                                />
+                            </template>
+                            <template #title>
+                                {{ post.title }}
+                            </template>
+                            <template #description>
+                                {{ post.summary }}
+                            </template>
+                        </Card>
+                    </li>
+                </ul>
+            </template>
+        </CardGrid>
+        <p v-else>
+            There are no posts available at this time!
+        </p>
+
+        <h2>From elsewhere on the web</h2>
+        <ul class="external-article-list">
+            <li>
+                <Article
+                    title="An Effective Python Environment: Making Yourself at Home"
+                    site="Real Python"
+                    link="https://realpython.com/effective-python-environment/"
+                />
+            </li>
+            <li>
+                <Article
+                    title="Deploying Features Under the Cover of Darkness"
+                    site="Build Smarter"
+                    link="https://medium.com/build-smarter/deploying-features-under-cover-of-darkness-f112ce444bba"
+                />
+            </li>
+            <li>
+                <Article
+                    title="Blazing fast Python"
+                    site="Build Smarter"
+                    link="https://medium.com/build-smarter/blazing-fast-python-40a2b25b0495"
+                />
+            </li>
+            <li>
+                <Article
+                    title="Consolidating Multiple Django Projects"
+                    site="Easy as Python"
+                    link="https://easyaspython.com/how-to-consolidate-multiple-django-projects-b7c9bb940a59"
+                />
+            </li>
+            <li>
+                <Article
+                    title="Mixins for Fun and Profit"
+                    site="Easy as Python"
+                    link="https://easyaspython.com/mixins-for-fun-and-profit-cb9962760556"
+                />
+            </li>
+            <li>
+                <Article
+                    title="Django's cached template loader"
+                    site="Build Smarter"
+                    link="https://medium.com/build-smarter/djangos-cached-template-loader-829f7b176e6f"
+                />
+            </li>
         </ul>
-      </template>
-    </CardGrid>
-    <p v-else>There are no posts available at this time!</p>
-  </main>
+    </main>
 </template>
 
 <script>
-import BaseLink from '@/components/BaseLink'
 import Card from '@/components/Card/Card'
 import CardGrid from '@/components/CardGrid'
 import ResponsiveImage from '@/components/ResponsiveImage'
+import Article from "../components/Article";
 
 const PAGE_TITLE = 'Blog posts'
 const PAGE_DESCRIPTION = 'Blog posts about (mostly) web application development by Dane Hillard'
@@ -43,7 +95,7 @@ const PAGE_DESCRIPTION = 'Blog posts about (mostly) web application development 
 export default {
   name: 'BlogPostList',
   components: {
-    BaseLink,
+    Article,
     Card,
     CardGrid,
     ResponsiveImage,
@@ -53,6 +105,9 @@ export default {
       loading: true,
       posts: {},
     }
+  },
+  created () {
+    this.fetchPosts()
   },
   methods: {
     fetchPosts () {
@@ -70,9 +125,6 @@ export default {
       const imageId = imageUrl.replace(butterPrefix, '')
       return `${butterPrefix}resize=width:${thumbnailSize},height:${thumbnailSize}/${imageId}`
     },
-  },
-  created () {
-    this.fetchPosts()
   },
   metaInfo: {
     title: PAGE_TITLE,
@@ -107,5 +159,9 @@ export default {
   @at-root #{&}__post {
     margin: 0;
   }
+}
+
+.external-article-list {
+  list-style: none;
 }
 </style>
