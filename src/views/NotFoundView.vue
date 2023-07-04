c3b7a76b6bf36f2779d028a64c625aaa148c63e5
Dane Hillard <github@danehillard.com>
2023-07-04 13:58:05 -0400

    Fix 404 routing (#239)

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { useHead } from '@unhead/vue'
+import BaseLink from '@/components/Link/BaseLink.vue'
+
+const pageDescription = 'This page does not exist'
+useHead({
+  title: 'Page not found',
+  meta: [
+    {
+      name: 'description',
+      content: pageDescription
+    },
+    {
+      property: 'og:description',
+      content: pageDescription
+    }
+  ]
+})
+</script>
+
+<template>
+  <main>
+    <h1>Page Not Found</h1>
+
+    <p>
+      It looks like the page you're looking for doesn't exist. Try going
+      <router-link :to="{ name: 'home' }" custom v-slot="{ navigate }">
+        <span @click="navigate" @keypress.enter="navigate" role="link">
+          <BaseLink href="/" :external="false">home</BaseLink>
+        </span>
+      </router-link>
+      and starting over.
+    </p>
+  </main>
+</template>
